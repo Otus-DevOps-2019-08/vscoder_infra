c4104ff5914325fb1ee982d138432ec3e67809da
Aleksey Koloskov <vsyscoder@gmail.com>
2019-09-23 17:44:05 +0300

    Attach startup script via startup-script-url

--- init.sh
+++ init.sh
@@ -13,16 +13,47 @@
 VM_NAME=reddit-app
 VM_TAGS=puma-server
 STARTUP_SCRIPT=startup.sh
+BUCKET=gs://vscoder-otus-hw4/
 
-# Generate startup script
-cat install_ruby.sh install_mongodb.sh deploy.sh > $STARTUP_SCRIPT
 
-# Create VM
-gcloud compute instances create $VM_NAME\
-  --boot-disk-size=10GB \
-  --image-family ubuntu-1604-lts \
-  --image-project=ubuntu-os-cloud \
-  --machine-type=g1-small \
-  --tags $VM_TAGS \
-  --restart-on-failure \
-  --metadata-from-file startup-script=$STARTUP_SCRIPT
+# Create temporary working directory
+TEMPWD=$(mktemp -d "${TMPDIR:-/tmp/}$(basename $0).XXXXXXXXXXXX")
+
+echo "*** Check instance '$VM_NAME' exists"
+gcloud compute instances describe $VM_NAME > $TEMPWD/vmdescr.yml
+
+
+# If instance not exists, create it
+if [ $? -ne 0 ]
+then
+  # Generate startup script
+  echo "*** Generate startup.sh"
+  cat install_ruby.sh install_mongodb.sh deploy.sh > $STARTUP_SCRIPT
+
+  # Upload startup script
+  echo "*** Create bucket"
+  gsutil mb $BUCKET || { echo "ERROR: can't create bucket. Exiting"; exit 100; }
+  echo "*** Upload startup script"
+  gsutil cp $STARTUP_SCRIPT ${BUCKET}${STARTUP_SCRIPT} || { echo "ERROR: can't upload $STARTUP_SCRIPT to ${BUCKET}${STARTUP_SCRIPT}. Exiting"; exit 100; }
+
+  # Create VM
+  echo "*** Create VM"
+  gcloud compute instances create $VM_NAME\
+    --boot-disk-size=10GB \
+    --image-family ubuntu-1604-lts \
+    --image-project=ubuntu-os-cloud \
+    --machine-type=g1-small \
+    --tags $VM_TAGS \
+    --restart-on-failure \
+    --metadata startup-script-url=${BUCKET}${STARTUP_SCRIPT}
+
+  gcloud compute instances describe $VM_NAME > $TEMPWD/vmdescr.yml
+fi
+
+# Get VM's external ip
+echo -n "VM external IP is "
+cat $TEMPWD/vmdescr.yml | grep "natIP: " | awk '{ print $2; }'
+
+# Remove temp file and dir
+rm $TEMPWD/vmdescr.yml
+rm -r $TEMPWD
