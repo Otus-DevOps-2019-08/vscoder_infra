f2864c9e5649496f8f544c01fc40deedea1b1c4b
Aleksey Koloskov <vsyscoder@gmail.com>
2019-09-25 14:59:43 +0300

    Add scripts/ directory with provisioning scripts

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+###
+# install_mongodb.sh
+# Installs and start mongodb-org on Ubuntu 16.04
+# author: Aleksey Koloskov <vsyscoder@gmail.com>
+###
+
+PKGS="mongodb-org"
+VER="3.2"
+KEYURL="https://www.mongodb.org/static/pgp/server-3.2.asc"
+
+echo "*** Start mongodb installation..."
+
+#
+# Install
+#
+echo "*** Get apt key"
+wget -qO - $KEYURL | apt-key add - || { echo "ERROR: Can't get apt key from '$KEYURL'! Exiting."; exit 21; }
+
+echo "*** Add mongodb reposityry"
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/$VER multiverse" | tee /etc/apt/sources.list.d/mongodb-org-${VER}.list || { echo "ERROR: Can't add apt repository! Exiting."; exit 22; }
+
+echo "*** Update apt cache"
+apt-get -qq update || { echo "ERROR: Can't add apt repository! Exiting."; exit 23; }
+
+echo "*** Install package: $PKGS"
+apt-get install -y $PKGS || { echo "ERROR: Can't install package '$PKGS'! Exiting."; exit 24; }
+
+#
+# Start and check
+#
+echo "*** Enable and run mongod.service"
+systemctl start mongod || { echo "ERROR: Can't start mongod.service! Exiting."; exit 130; }
+systemctl enable mongod || { echo "ERROR: Can't enable mongod.service! Exiting."; exit 131; }
+systemctl --no-pager status mongod || { echo "ERROR: Service mongod is not running! Exiting."; exit 132; }
+
+echo "*** Complete. Service mongod installed and running."
---
+++ packer/scripts/install_ruby.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+###
+# install_ruby.sh
+# Installs latest ruby-full and ruby-bundler on Ubuntu 16.04
+# author: Aleksey Koloskov <vsyscoder@gmail.com>
+###
+
+PKGS="ruby-full ruby-bundler build-essential"
+
+echo "*** Start ruby and bundler installation..."
+
+#
+# Install
+#
+echo "*** Update apt cache"
+apt-get -qq update || { echo "ERROR: Can't add apt repository! Exiting."; exit 13; }
+
+echo "*** Install package: $PKGS"
+apt-get install -y $PKGS || { echo "ERROR: Can't install package '$PKGS'! Exiting."; exit 14; }
+
+#
+# Check
+#
+ruby -v || { echo "ERROR: Can't check ruby version! Exiting."; exit 128; }
+bundler -v || { echo "ERROR: Can't check bundler version! Exiting."; exit 129; }
+
+echo "*** Complete. Ruby and bundler installed succefully."
