23ccce6dcd667116122254702f88006959174290
Aleksey Koloskov <vsyscoder@gmail.com>
2019-09-29 22:49:18 +0300

    Update terraform/main.tf Add project-wide public ssh-key for user 'appuser1'

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,11 @@ provider "google" {
   region  = var.region
 }
 
+resource "google_compute_project_metadata_item" "sshkey-appuser1" {
+  key = "ssh-keys"
+  value = "appuser1:${file(var.public_key_path)}"
+}
+
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
