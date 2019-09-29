ebe5ac2612937a279158611e525fb526386ede2f
Aleksey Koloskov <vsyscoder@gmail.com>
2019-09-29 19:58:12 +0300

    Update terraform/variables.tf Add variable private_key_file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,12 +3,13 @@ variable project {
 }
 variable region {
   description = "Region"
-  # Значение по умолчанию
   default = "europe-west1"
 }
 variable public_key_path {
-  # Описание переменной
   description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+  description = "Path to the private key used for provisioners"
 }
 variable disk_image {
   description = "Disk image"
