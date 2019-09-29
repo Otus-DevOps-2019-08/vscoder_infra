88fb60ee2f0fcd3bcb1c05c5b41d54b3f8239d94
Aleksey Koloskov <vsyscoder@gmail.com>
2019-09-29 20:04:27 +0300

    Update terraform/variables.tf Add variable 'zone'

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,6 +5,10 @@ variable region {
   description = "Region"
   default = "europe-west1"
 }
+variable zone {
+  description = "Zone"
+  default = "europe-west1-d"
+}
 variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
