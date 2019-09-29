dc676c7911ea46d6ce5d67f85498f7e1f42e2903
Aleksey Koloskov <vsyscoder@gmail.com>
2019-09-29 14:49:46 +0300

    Update terraform/main.tf Apply google_compute_firewall rule 'firewall_puma' to google_compute_instance 'app' via tag 'reddit-app'

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,7 @@ resource "google_compute_instance" "app" {
   name = "reddit-app"
   machine_type = "g1-small"
   zone = "europe-west1-d"
+  tags = ["reddit-app"]
   boot_disk {
     initialize_params {
       image = "reddit-base"
