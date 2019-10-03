fdc194a77c54fb0507d9a57536b5f9c4aa2562a2
Aleksey Koloskov <vsyscoder@gmail.com>
2019-10-03 18:10:58 +0300

    Update terraform/lb.tf Fix some mistakes, add object dependencies, add small improvements.

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,12 +1,13 @@
 resource "google_compute_instance_group" "app_instance_group" {
-  name = "app-instance-group"
-  zone = var.zone
-  instances = [
-    "${google_compute_instance.app.self_link}",
-  ]
+  name      = "app-instance-group"
+  zone      = var.zone
+  instances = [for s in values(google_compute_instance.app)[*].self_link : s]
   named_port {
     name = "http"
     port = "9292"
+  }
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -18,18 +19,24 @@ resource "google_compute_health_check" "http-health-check" {
 
   http_health_check {
     port_name = "http"
+    port      = 9292
   }
 }
 
 resource "google_compute_backend_service" "app_home" {
-  name = "app-backend"
+  name = "app-home"
   health_checks = [
     "${google_compute_health_check.http-health-check.self_link}",
   ]
   port_name = "http"
+  protocol  = "HTTP"
   backend {
     group = "${google_compute_instance_group.app_instance_group.self_link}"
   }
+  depends_on = [
+    google_compute_instance_group.app_instance_group,
+    google_compute_health_check.http-health-check
+  ]
 }
 
 resource "google_compute_url_map" "app_urlmap" {
@@ -48,21 +55,24 @@ resource "google_compute_url_map" "app_urlmap" {
     default_service = "${google_compute_backend_service.app_home.self_link}"
 
     path_rule {
-      paths   = ["/*"]
+      paths   = ["/", "/*"]
       service = "${google_compute_backend_service.app_home.self_link}"
     }
   }
+  depends_on = [google_compute_backend_service.app_home]
 }
 
 resource "google_compute_target_http_proxy" "app_proxy" {
-  name    = "app-proxy"
-  url_map = "${google_compute_url_map.app_urlmap.self_link}"
+  name       = "app-proxy"
+  url_map    = "${google_compute_url_map.app_urlmap.self_link}"
+  depends_on = [google_compute_url_map.app_urlmap]
 }
 
 resource "google_compute_global_forwarding_rule" "app_forwarding_rule" {
   name       = "app-global-rule"
   target     = "${google_compute_target_http_proxy.app_proxy.self_link}"
   port_range = "80"
+  depends_on = [google_compute_target_http_proxy.app_proxy]
 }
 
 # output "app_external_ip" {
