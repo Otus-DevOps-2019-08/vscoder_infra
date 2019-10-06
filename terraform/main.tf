abf92b133645b9225d765ad2dc66cf4dcff47834
Aleksey Koloskov <vsyscoder@gmail.com>
2019-10-06 10:43:50 +0300

    Update terraform/main.tf Add resource google_compute_firewall.firewall_ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,3 +62,16 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name        = "default-allow-ssh"
+  description = "Allow access to ssh to all instances"
+  network     = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
