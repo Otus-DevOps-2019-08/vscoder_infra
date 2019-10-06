1fe1a00910ad7cd4f97523c75bc0a18b0aa4640d
Aleksey Koloskov <vsyscoder@gmail.com>
2019-10-06 10:50:59 +0300

    Update terraform/main.tf Link google_compute_instance.app network_interface access_config nat_ip to google_compute_address.app_ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,9 @@ resource "google_compute_instance" "app" {
 
   network_interface {
     network = "default"
-    access_config {}
+    access_config {
+      nat_ip = google_compute_address.app_ip.address
+    }
   }
 
   provisioner "file" {
